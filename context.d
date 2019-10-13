f707c85fc3b8fa05b49b7957ddea460963acfc27
jj1lis <rinta.nambuline205@gmail.com>
2019-10-14 02:00:54 +0900

    [change]Complete with some exception
    There aren't completed yet:
    Pos & Subpos
    >>culculate score of texts<<

--- context.d
+++ context.d
@@ -3,53 +3,10 @@ import std.file;
 import std.string;
 import std.conv;
 
-class NoTextNumberException:Exception{
-    this(int num){
-        string msg="Text Number "~to!string(num)~" not found.";
-        super(msg);
-    }
-}
-
-class stringToIntException:Exception{
-    this(string score,int stc){
-        string msg=score~" cannot be converted from \'string\' to \'int\' in sentence "~to!string(stc)~".";
-        super(msg);
-    }
-}
-
 enum Type{
     t_word,
-    t_sentence,
+    t_stc,
     t_text,
-}
-
-string typeToString(Type type){
-    switch(type){
-        case Type.t_word:
-            return "Word";
-            //break;
-        case Type.t_sentence:
-            return "Sentence";
-            //break;
-        case Type.t_text:
-            return "Text";
-            //break;
-        default:
-            return "Unknown";
-    }
-}
-
-class scoreException:Exception{
-    this(Type item,int num){
-        string cursor=item.typeToString~" No."~to!string(num);
-        super(cursor~": score must be in range -100 ~ 100.");
-    }
-}
-
-class argumentNumberException:Exception{
-    this(string reason){
-        super("Invalid argument: "~reason~".");
-    }
 }
 
 enum Pos{
@@ -64,6 +21,105 @@ enum Subpos{
     unknown,
 };
 
+class NoTextNumberException:Exception{
+    this(int num){
+        string msg="Text Number "~to!string(num)~" not found.";
+        super(msg);
+    }
+}
+
+class stringToIntException:Exception{
+    this(string str,int text){
+        string msg="\""~str~"\" cannot be converted from \'string\' to \'int\' in text "~to!string(text)~".";
+        super(msg);
+    }
+
+    this(string str,int stc,int text){
+        string msg="\""~str~"\" cannot be converted from \'string\' to \'int\' in sentence "~to!string(stc)~",text "~to!string(text)~".";
+        super(msg);
+    }
+
+    this(string str,int word,int stc,int text){
+        string msg="\""~str~"\" cannot be converted from \'string\' to \'int\' in word "~to!string(word)~",sentence "~to!string(stc)~",text "~to!string(text)~".";
+        super(msg);
+    }
+}
+
+string TypeToString(Type type){
+    switch(type){
+        case Type.t_word:
+            return "Word";
+            //break;
+        case Type.t_stc:
+            return "Sentence";
+            //break;
+        case Type.t_text:
+            return "Text";
+            //break;
+        default:
+            return "Unknown";
+    }
+}
+
+class scoreException:Exception{
+    this(int text){
+        string cursor="text "~to!string(text);
+        super(cursor~": score must be in range -100 ~ 100.");
+    }
+    this(int stc,int text){
+        string cursor="sentence "~to!string(stc)~",text "~to!string(text);
+        super(cursor~": score must be in range -100 ~ 100.");
+    }
+    this(int word,int stc,int text){
+        string cursor="word "~to!string(word)~",sentence "~to!string(stc)~",text "~to!string(text);
+        super(cursor~": score must be in range -100 ~ 100.");
+    }
+}
+
+class argumentNumberException:Exception{
+    this(string reason){
+        super("Invalid argument: "~reason~".");
+    }
+}
+Pos stringToPos(string str_pos){
+    switch(str_pos){
+        case "dammy":
+            return Pos.dammy;//TODO
+            break;
+        default:
+            return Pos.unknown;
+    }
+}
+
+string PosToString(Pos pos_str){
+    switch(pos_str){
+        case Pos.dammy:
+            return "dammy";//TODO
+            break;
+        default:
+            return "unkonow";
+    }
+}
+
+Subpos stringToSubpos(string str_subpos){
+    switch(str_subpos){
+        case "dammy":
+            return Subpos.dammy;//TODO
+            break;
+        default:
+            return Subpos.unknown;
+    }
+}
+
+string SubposToString(Subpos subpos_str){
+    switch(subpos_str){
+        case Subpos.dammy:
+            return "dammy";//TODO
+            break;
+        default:
+            return "unkonow";
+    }
+}
 
 class Meta{
     Type type;
@@ -72,7 +128,7 @@ class Meta{
         type=target;
         num=nu;
     }
-    
+
     Type getType(){
         return type;
     }
@@ -82,38 +138,32 @@ class Meta{
 }
 
 class Word:Meta{
-    private string word;
+    private string mor; //morpheme
     private Pos pos;
     private Subpos subpos;
     private string base;
 
     this(string line_word,int number){
         auto record=line_word.split(",");
-        word=record[0];
-        pos=checkPos(record[1]);
-        subpos=checkSubpos(record[2]);
+        mor=record[0];
+        pos=stringToPos(record[1]);
+        subpos=stringToSubpos(record[2]);
         base=record[3];
         super(Type.t_word,number);
     }
 
-    private Pos checkPos(string str_pos){
-        switch(str_pos){
-            case "":
-                return Pos.dammy;//TODO
-                break;
-            default:
-                return Pos.unknown;
-        }
-    }
 
-    private Subpos checkSubpos(string str_subpos){
-        switch(str_subpos){
-            case "":
-                return Subpos.dammy;//TODO
-                break;
-            default:
-                return Subpos.unknown;
-        }
+    string getMor(){
+        return mor;
+    }
+    Pos getPos(){
+        return pos;
+    }
+    Subpos getSubpos(){
+        return subpos;
+    }
+    string getBase(){
+        return base;
     }
 };
 
@@ -127,7 +177,7 @@ class Sentence:Meta{
         for(int cnt=0;cnt<lines.length;cnt++){
             words[cnt]=new Word(lines[cnt],cnt);
         }
-        super(Type.t_sentence,number);
+        super(Type.t_stc,number);
     }
 
     int getScore(){
@@ -151,9 +201,12 @@ class Text:Meta{
             }else{
                 int score_stc;
                 try{
-                score_stc=to!int(lines[cnt].split(",")[1]);
+                    score_stc=to!int(lines[cnt].split(",")[1]);
                 }catch{
-                    throw new stringToIntException(lines[cnt].split(",")[1],cnt_stc);
+                    throw new stringToIntException(lines[cnt].split(",")[1],cnt_stc,num);
+                }
+                if(score_stc<-100||score_stc>100){
+                    throw new scoreException(cnt_stc,number);
                 }
                 sentences.length++;
                 sentences[cnt_stc]=new Sentence(tmp_stc,score_stc,cnt_stc);
@@ -180,7 +233,7 @@ class Text:Meta{
 
 string[] devideFileByLine(string filename){
     try{
-    return readText(filename).splitLines;
+        return readText(filename).splitLines;
     }catch(FileException fe){
         throw new FileException(filename,"Failed to open File");
     }
@@ -210,6 +263,22 @@ int calculateTextScore(Text target){
     return 0;//DENUG
 }
 
+void writeText(string writefile,Text target){
+    for(int cnt_stc=0;cnt_stc<target.sentences.length;cnt_stc++){
+        Sentence target_stc=target.sentences[cnt_stc];
+        for(int cnt_word=0;cnt_word<target_stc.words.length;cnt_word++){
+            Word target_word=target_stc.words[cnt_word];
+            append(writefile,target_word.getMor()~","~target_word.getPos.PosToString~
+                    ","~target_word.getSubpos.SubposToString~","~
+                    target_word.getBase()~"\n");
+        } 
+        append(writefile,"%,"~to!string(target_stc.getNumber())~
+                ","~to!string(target_stc.getScore)~"\n");
+    }
+    append(writefile,"#,"~to!string(target.getNumber())~
+            ","~to!string(target.getScore)~"\n");
+}
+
 void main(string[] args){
     if(args.length<2){
         stderr.writeln("erorr: "~new argumentNumberException("argument is too little").msg);
@@ -220,7 +289,7 @@ void main(string[] args){
         int read_text_num=0;
         Text text;
         try{
-        text=new Text(separateText(devideFileByLine(args[1]),read_text_num),read_text_num);
+            text=new Text(separateText(devideFileByLine(args[1]),read_text_num),read_text_num);
         }catch(FileException fe){
             stderr.writeln("erorr: "~fe.msg);
         }catch(NoTextNumberException ntne){
@@ -228,7 +297,9 @@ void main(string[] args){
         }catch(stringToIntException stie){
             stderr.writeln("error: "~stie.msg);
         }
-        
+        text.setScore(calculateTextScore(text));
+        writeText(args[1]~".ctx",text);
+
     }
 
     //"Hello!!".writeln;
