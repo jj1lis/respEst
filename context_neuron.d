ce38c8b5db284a6b71b27be03efa44f43ff0a8c5
jj1lis <rinta.nambuline205@gmail.com>
2019-11-04 21:54:40 +0900

    [fix]:context_neuron.d

--- context_neuron.d
+++ context_neuron.d
@@ -22,22 +22,46 @@ string LayerToString(Layer l) {
     }
 }
 
-abstract class Neuron{
-    Layer layer;
-    int neuron_number;
+class Neuron{
+    private Layer layer;
+    private int neuron_number;
     private real[] inputs;
     private real[] weights;
     private real threshold;
     private real integrated_value;
     private real output;
 
-    real transferFunc(real){return 0;};//abstract
-    
     this(Layer _layer,int neu_num,
             real _weights[],real _threshold){
         layer=_layer;
         neuron_number=neu_num;
         weights=_weights;
         threshold=_threshold;
+    }
+
+    Layer getLayer(){
+        return layer;
+    }
+
+    int getNumber(){
+        return neuron_number;
+    }
+
+    abstract real transferFunc(real);//abstract
+
+    abstract void modify();
+}
+
+class InputNeuron:Neuron{
+    this(int num,real[] _weights,real _threshold){
+        super(Layer.input,num,_weights,_threshold);
+    }
+
+    override real transferFunc(real u){
+        return u;   //TODO
+    }
+
+    override void modify(){
+        //TODO
     }
 }
