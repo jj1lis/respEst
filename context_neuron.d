4a40490a8d27405c79d4187a7458324c1715c9a3
jj1lis <rinta.nambuline205@gmail.com>
2019-11-09 22:54:08 +0900

    aloha

--- context_neuron.d
+++ context_neuron.d
@@ -1,3 +1,4 @@
+import std.file;
 import context_exception;
 
 enum Layer{
@@ -47,7 +48,7 @@ class Neuron{
         return neuron_number;
     }
 
-    abstract real transferFunc(real);//abstract
+    abstract real transferFunc(real);
 
     abstract void modify();
 }
@@ -63,5 +64,77 @@ class InputNeuron:Neuron{
 
     override void modify(){
         //TODO
+    }
+}
+
+class MiddleNeuron:Neuron{
+    this(int num,real[] _weights,real _threshold){
+        super(Layer.input,num,_weights,_threshold);
+    }
+
+    override real transferFunc(real u){
+        return u;   //TODO
+    }
+
+    override void modify(){
+        //TODO
+    }
+}
+
+class OutputNeuron:Neuron{
+    this(int num,real[] _weights,real _threshold){
+        super(Layer.input,num,_weights,_threshold);
+    }
+
+    override real transferFunc(real u){
+        return u;   //TODO
+    }
+
+    override void modify(){
+        //TODO
+    }
+}
+
+string[] pickTag(string[] str,string tag,char marker){
+    char marker_end;
+    switch(marker){
+        case '<':
+            marker_end='>';
+            break;
+        case '[':
+            marker_end=']';
+            break;
+        default:
+            assert(0);
+    }
+    string start=marker~tag~marker_end;
+    string end=marker~'/'~tag~marker_end;
+    string[] content;
+    for//TODO
+}
+
+class Stat{
+    this(string read_file){
+        string file_whole[];
+        try{
+            file_whole=readText(read_file).splitLines;
+        }catch(FileException fe){
+            throw new FileException(read_file,"Faild to open File");
+        }
+        for(int cnt=0;cnt<file_whole.length;cnt++){
+            if(indexOf(file_whole[cnt],"[meta]"))
+        }
+    }
+}
+
+class Network{
+    private InputNeuron[] ineuron;
+    private MiddleNeuron[] mneuron;
+    private OutputNeuron[] oneuron;
+    private int howmany_middlelayer;
+    private Stat stat;
+
+    this(string filename,int how_midlay,int how_ineu,int how_mneu,int how_oneu){
+        stat=new Stat(filename);
     }
 }
