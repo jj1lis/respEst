37628a725e2273a546ebdc0a683d527ffb7c2a53
jj1lis <rinta.nambuline205@gmail.com>
2019-12-10 20:22:52 +0900

    [change]:started to use dub.

---
+++ context/source/utils.d
@@ -0,0 +1,93 @@
+module context.utils;
+
+import std.stdio;
+import std.file;
+import std.string;
+import std.conv;
+import context.exception;
+import context.text;
+import context.calc;
+import context.pos;
+
+
+string[] devideFileByLine(string filename){
+    try{
+        return readText(filename).splitLines;
+    }catch(FileException fe){
+        throw new FileException(filename,"failed to open file");
+    }
+}
+
+string[] separateText(string[] file_lines,int text_number){//todo!:this dumps myst bugs
+    string[] tmp_text=new string[0];
+    int cnt_text=0;
+    for(int cnt=0;cnt<file_lines.length;cnt++){
+        if(file_lines[cnt].split(",")[0]!="#"){
+            tmp_text.length++;
+            tmp_text[cnt_text]=file_lines[cnt];
+            cnt_text++;
+        }else if(to!int(file_lines[cnt].split(",")[1])==text_number){
+            return tmp_text;
+        }else{
+            tmp_text.length=0;
+            cnt_text=0;
+        }
+    }
+    throw new NoTextNumberException(text_number);
+}
+
+void writeText(string writefile,Text target){
+    try{
+        if(exists(writefile)&&isFile(writefile)){
+            remove(writefile);
+        }
+    }catch(FileException fe){
+        stderr.writeln("error: "~fe.msg);
+    }
+
+    for(int cnt_sentence=0;cnt_sentence<target.sentences.length;cnt_sentence++){
+        Sentence target_sentence=target.sentences[cnt_sentence];
+        for(int cnt_phrase=0;cnt_phrase<target_sentence.phrases.length;cnt_phrase++){
+            Phrase target_phrase=target_sentence.phrases[cnt_phrase]; 
+            for(int cnt_word=0;cnt_word<target_phrase.words.length;cnt_word++){
+                Word target_word=target_phrase.words[cnt_word];
+                append(writefile,target_word.morpheme()~","~target_word.poses.pos.to!string~
+                        ","~target_word.poses.subpos1.to!string~","~target_word.poses.subpos2.to!string~
+                        ","~target_word.poses.subpos3.to!string~","~target_word.base~"\n");
+            }
+            append(writefile,"$,"~to!string(target_phrase.number)~
+                    ","~to!string(target_phrase.dependency)~"\n");
+        }
+        append(writefile,"%,"~to!string(target_sentence.number)~
+                ","~to!string(target_sentence.score)~"\n");
+    }
+    append(writefile,"#,"~to!string(target.number)~
+            ","~to!string(target.score)~"\n");
+}
+
+void writeAnalysis(string alsfile,Text target){
+    try{
+        if(exists(alsfile)&&isFile(alsfile)){
+            remove(alsfile);
+        }
+    }catch(FileException fe){
+        stderr.writeln("error: "~fe.msg);
+    }
+
+}
+
+void debugSpace(Text target){
+    string[] text=new string[0];
+    foreach(Sentence s;target.sentences){
+        foreach(Phrase p;s.phrases){
+            foreach(Word w;p.words){
+                text~=w.morpheme;
+            }
+        }
+    }
+    foreach(string s;text){
+        s.write;
+    }
+    "\n".write;
+}
+
